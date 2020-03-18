99ea212976f7a7020c783ef7b86b301c3e32aa85
Dennie Nguyen <streamvillecanada@gmail.com>
2020-03-18 18:10:14 -0400

    Initial Commit

---
+++ BingeBot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BingeBot
+//
+//  Created by Duong Nguyen on 2020-03-18.
+//  Copyright © 2020 Streamville Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
