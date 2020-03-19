08c0d9c193290408390d9f0257d3031e1418fdaf
Dennie Nguyen <streamvillecanada@gmail.com>
2020-03-19 13:42:43 -0400

    Initial commit

--- BingeBot/ViewController.swift
+++ BingeBot/ViewController.swift
@@ -10,10 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var showsLabel: UILabel!
+    
+    @IBOutlet weak var randomShowLbl: UILabel!
+    
+    @IBOutlet weak var bingeBotLbl: UILabel!
+    
+    @IBOutlet weak var addShowInput: UITextField!
+    
+    @IBOutlet weak var addShowBtn: UIButton!
+    
+    @IBOutlet weak var randomShowStackView: UIStackView!
+    
+    @IBOutlet weak var addShowStackView: UIStackView!
+    
+    @IBOutlet weak var showStackView: UIStackView!
+    
+    var shows: [String] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        showStackView.isHidden = true
+        randomShowStackView.isHidden = true
     }
+    
+    func updateShowLabel(){
+        showsLabel.text = shows.joined(separator: ", ")
+    }
+    
+    @IBAction func bingeShowBtnPressed(_ sender: Any) {
+        randomShowLbl.text = shows.randomElement()
+        randomShowLbl.isHidden = false
+        bingeBotLbl.isHidden = false 
+    }
+    
+    
+    
+    
+    @IBAction func addShowBtnPressed(_ sender: Any) {
+        guard let showName = addShowInput.text else {return}
+        shows.append(showName)
+        updateShowLabel()
+        addShowInput.text = ""
+        showStackView.isHidden = false
+        
+        if shows.count > 1{
+            randomShowStackView.isHidden = false
+            bingeBotLbl.isHidden = true
+            randomShowLbl.isHidden = true
+        }
+    }
+    
+    
+    
 
 
 }
